83db01e74c2c44a9beb2e4949e4d79dbc907d4b9
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2021-08-01 16:15:57 +0400

    database swap

--- config/database.js
+++ config/database.js
@@ -4,12 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'root'),
+		ssl: env('DATABASE_SSL', false),
+        //schema: env('DATABASE_SCHEMA', 'public'), // Not Required
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
